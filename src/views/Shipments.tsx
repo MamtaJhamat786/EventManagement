67f22e196951e33bba7c7e73582ac7c8fb7eadd1
Mamta <mamta@erply.com>
2023-08-13 21:35:34 +0300

    AD Test Task

---
+++ src/views/Shipments.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Typography} from "@mui/material";
+
+const Products:React.FC=()=>{
+    return(
+        <Typography> Products</Typography>
+    )
+}
+export default Products;
